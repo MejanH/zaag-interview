775915ff0a46ca924e2a95871f42853d9c42e157
MejanH <muhammad.mejanul.haque@gmail.com>
2021-12-12 13:58:49 +0600

    test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { renderWithRouter } from "./tests/utils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("Home page should have header, a textfield and a submit button", () => {
+  renderWithRouter(<App />);
+  const appHeader = screen.getByText("Zaag Interview");
+  const submitButton = screen.getByRole("button");
+
+  expect(appHeader).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
 });
